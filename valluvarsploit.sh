c6b8c497bfca01afc6aa9ca90f119ab513ef02be
ValluvarSploit <alexandar.thangavel01@gmail.com>
2022-08-02 16:38:05 +0530

    update

--- valluvarsploit.sh
+++ valluvarsploit.sh
@@ -7,7 +7,12 @@ SERVER_PATH="$DOMAIN_PATH/servers.txt"
 mkdir -p $DOMAIN_PATH
 
 
-#./subdomain_enumeration.sh $DOMAIN
-#./httprobe.sh $SUBDOMAIN_PATH $DOMAIN_PATH
-#./nuclei.sh $SERVER_PATH $DOMAIN_PATH
-./sensitive_file.sh $SERVER_PATH $DOMAIN_PATH
+if [ $2 -eq 1]; then
+	./subdomain_enumeration.sh $DOMAIN
+elif [ $2 -eq 2]; then
+	./httprobe.sh $SUBDOMAIN_PATH $DOMAIN_PATH
+elif [ $2 -eq 3]; then
+	./nuclei.sh $SERVER_PATH $DOMAIN_PATH
+elif [ $2 -eq 4 ]; then
+	./sensitive_file.sh $SERVER_PATH $DOMAIN_PATH
+fi
