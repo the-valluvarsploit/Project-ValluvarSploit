859bc4fb57f7c4d392030e3bd798f54d4a873962
ValluvarSploit <alexandar.thangavel01@gmail.com>
2022-08-02 14:31:42 +0530

    update

--- nuclei.sh
+++ nuclei.sh
@@ -3,4 +3,7 @@
 
 ##nuclei -l $1 -t / -o $2/nuclei_scan.txt -stats -vv
 #nuclei -l $1 -t cves -o $2/nuclei_cve.txt -stats -vv
-nuclei -l $1 -t misconfiguration/ -o $2/nuclei_misconfig.txt -stats -vv
+#nuclei -l $1 -t misconfiguration/ -o $2/nuclei_misconfig.txt -stats -vv
+
+
+nuclei -l $1 -s critical -o $2/nuclei_critical.txt -stats -vv
