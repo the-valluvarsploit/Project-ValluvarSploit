68acbb491c8c712512d87cf9cb03dac80335e4cd
ValluvarSploit <alexandar.thangavel01@gmail.com>
2022-08-03 09:44:03 +0530

    update

--- subdomain_enumeration.sh
+++ subdomain_enumeration.sh
@@ -33,4 +33,6 @@ if [ ! -d "$SUBDOMAIN" ]; then
         cat $SUBDOMAIN/subdomain_temp.txt $SUBDOMAIN/puredns_alter.txt | anew $SUBDOMAIN/subdomains.txt
 
         puredns resolve $SUBDOMAIN/subdomains.txt --resolvers $TOOLS_PATH/resolvers.txt  -w $SUBDOMAIN/subdomains_alive.txt
+
+	rm $SUBDOMAIN/amass.txt $SUBDOMAIN/subfinder.txt $SUBDOMAIN/findomain.txt $SUBDOMAIN/assetfinder.txt $SUBDOMAIN/puredns_brute.txt $SUBDOMAIN/subdomain_temp.txt $SUBDOMAIN/puredns_temp.txt $SUBDOMAIN/gotator-perm.txt $SUBDOMAIN/puredns_alter.txt
 fi
